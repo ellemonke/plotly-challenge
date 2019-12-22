2e8f4c9dfd4d36e4adf6a367314600ad00021c74
ellemonke <ellemonke@gmail.com>
2019-12-21 21:30:30 -0800

    all charts plus gauge but need styling

--- static/js/app.js
+++ static/js/app.js
@@ -41,8 +41,38 @@ function demoTable(name) {
                     bbtype: ${person.bbtype}<br>
                     wfreq: ${person.wfreq}<br>
                 `);
+
+                // GAUGE CHART
+                var gaugeData = [{
+                    domain: { x: [0, 9], y: [0, 9] },
+                    value: person.wfreq,
+                    title: { text: "Belly Button Washing Frequency Scrubs Per Week" },
+                    type: "indicator",
+                    mode: "gauge",
+                    gauge: {
+                        axis: { range: [null, 9] },
+                        steps: [
+                          { range: [0, 1], color: "rgb(247, 242, 235)" },
+                          { range: [1, 2], color: "rgb(236, 245, 227)" },
+                          { range: [2, 3], color: "rgb(225, 247, 209)" },
+                          { range: [3, 4], color: "rgb(214, 250, 181)" },
+                          { range: [4, 5], color: "rgb(204, 255, 153)" },
+                          { range: [5, 6], color: "rgb(189, 238, 150)" },
+                          { range: [6, 7], color: "rgb(174, 222, 148)" },
+                          { range: [7, 8], color: "rgb(158, 205, 146)" },
+                          { range: [8, 9], color: "rgb(143, 188, 143)" }
+                        ],
+                        borderwidth: 0
+                    }                    
+                }];
+                
+                Plotly.newPlot('gauge', gaugeData);
+
             };
         });
+
+
+
     });
 };
 
@@ -58,40 +88,54 @@ function plotCharts(name) {
         // Check for specified person in samples
         data.samples.forEach(sample => {
             if (parseInt(sample.id) === parseInt(name)) {
-
-                // Set plot values
-                var ids = sample.otu_ids;
-                var values = sample.sample_values;
-                var labels = sample.otu_labels;
-
+                
                 // BAR CHART
+                var topIds = sample.otu_ids.slice(0,10);
+                var topValues = sample.sample_values.slice(0,10);
+                var topLabels = sample.otu_labels.slice(0,10);
+
+                var topIdsStr = topIds.map(id => `OTU ${id}`);
+
                 barData = [{
-                    y: ids,
-                    x: values,
-                    text: labels,
+                    y: topIdsStr,
+                    x: topValues,
+                    text: topLabels,
                     type: "bar", 
-                    orientation: "h"
+                    orientation: "h",
+                    transforms: [{
+                        type: 'sort',
+                        target: 'y',
+                        order: 'descending'
+                    }]
+                    // marker: {line: {width: 15}}
                 }];
 
                 Plotly.newPlot("bar", barData);    
 
                 // BUBBLE CHART
+                var ids = sample.otu_ids;
+                var values = sample.sample_values;
+                var labels = sample.otu_labels;
+
                 bubbleData = [{
                     x: ids,
                     y: values,
                     text: labels, 
                     mode: "markers",
                     marker: {
-                        color: "green",
+                        color: ids,
                         size: values
-                    }
+                    },
+                    // colorscale: "YIGnBu"
                 }];
 
                 var bubbleLayout = {
-                    xaxis: {title: "OTU ID"}
+                    xaxis: {title: "OTU ID"},
+                    hovermode: "closest"
                 };
 
                 Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
             };   
         });
     });    
